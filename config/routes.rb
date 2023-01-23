1bbf0a2815fa2cba7f3c9ab9c9fa55476bd6c983
jun218391 <122144759+jun218391@users.noreply.github.com>
2023-01-23 20:34:54 +0000

    [finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
+  resources :post_images,only:[:new, :index, :show]
+  
   devise_for :users
   root to: 'homes#top'
   
