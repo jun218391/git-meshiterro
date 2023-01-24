f867ef1f031f8aa599f446e822613ab8dd039953
jun218391 <122144759+jun218391@users.noreply.github.com>
2023-01-24 06:32:06 +0000

    [finish]4-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # get 'post_images/new'
-  # get 'post_images/index'
-  # get 'post_images/show'
-  resources :post_images,only:[:new, :create, :index, :show]
-  
+ 
   root to: 'homes#top'
   devise_for :users
-  
-  
   get 'homes/about' => 'homes#about', as: 'about'
+  
+  resources :post_images,only:[:new, :create, :destroy, :index, :show]
+  
+
 end
